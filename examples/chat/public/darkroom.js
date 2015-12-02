6c953e5961aa0a75a4a12548bfa4fafc7c66cf31
Crystal <omaracrystal@gmail.com>
2015-12-01 22:23:18 -0700

    fixing header and nav

--- examples/chat/public/darkroom.js
+++ examples/chat/public/darkroom.js
@@ -1,27 +1,27 @@
 (function darkroom(){
-  function flashlightOff() {
-    $(this).css({
-        '-webkit-mask-image': ''
-      });
-    }
-
-    function flashlight(e) {
-      var mouseX = e.pageX - $(this).offset().left;
-      var mouseY = e.pageY - $(this).offset().top;
+    function flashlightOff() {
       $(this).css({
-        '-webkit-mask-image': 'radial-gradient(circle 60px at ' + mouseX + 'px ' + mouseY + 'px, rgba(255,255,255,1) 60%, rgba(255,255,255,0) 100%)',
-        'cursor': 'none'
+          '-webkit-mask-image': ''
+        });
+      }
+
+      function flashlight(e) {
+        var mouseX = e.pageX - $(this).offset().left;
+        var mouseY = e.pageY - $(this).offset().top;
+        $(this).css({
+          '-webkit-mask-image': 'radial-gradient(circle 60px at ' + mouseX + 'px ' + mouseY + 'px, rgba(255,255,255,1) 60%, rgba(255,255,255,0) 100%)',
+          'cursor': 'none'
+        });
+
+      }
+
+      function updateImg() {
+        $('.masked').attr('src', 'http://www.placekitten.com/' + $(window).width() + '/' + $(window).height());
+      }
+
+
+      $('.masked').on({
+        'mousemove': flashlight,
+        'mouseleave': flashlightOff
       });
-
-    }
-
-    function updateImg() {
-      $('.masked').attr('src', 'http://www.placekitten.com/' + $(window).width() + '/' + $(window).height());
-    }
-
-
-    $('.masked').on({
-      'mousemove': flashlight,
-      'mouseleave': flashlightOff
-    });
-})();
+  })();
