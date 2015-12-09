bcd0f8192cf4ff712b3ba0b7df8c5937084c6f18
Crystal <omaracrystal@gmail.com>
2015-12-08 17:03:23 -0700

    changed https in n.io to //

--- public/js/general/darkroom/nio/nio2.js
+++ public/js/general/darkroom/nio/nio2.js
@@ -3,7 +3,7 @@ function nio2() {
 var socket = io();
 
 nio.source.socketio(
- 'http://brand.nioinstances.com',
+ '//brand.nioinstances.com',
  ['crystal_mobile_data', 'crystal_global_shake_count']).pipe(nio.log());
 
 
