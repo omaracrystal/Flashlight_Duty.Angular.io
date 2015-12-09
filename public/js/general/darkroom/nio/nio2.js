84bd1de68da22ec9ed94e05b572192c2e1bced22
Crystal <omaracrystal@gmail.com>
2015-12-08 17:42:15 -0700

    changed n.io

--- public/js/general/darkroom/nio/nio2.js
+++ public/js/general/darkroom/nio/nio2.js
@@ -3,7 +3,7 @@ function nio2() {
 var socket = io();
 
 nio.source.socketio(
- '//brand.nioinstances.com',
+ 'http://brand.nioinstances.com',
  ['crystal_mobile_data', 'crystal_global_shake_count']).pipe(nio.log());
 
 
